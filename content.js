6924a6c7514795fc0c4b65c0615b1f93e1197c62
Vinit <vinitsorathia1@gmail.com>
2025-01-21 13:16:10 +0530

    add sync storage

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 const bookmarkURL = chrome.runtime.getURL("assets/bookmark.png");
-
+const PROBLEM_KEY="PROBLEM_KEY";
 window.addEventListener("load",addBookmarkButton);
 
 function addBookmarkButton(){
@@ -12,4 +12,45 @@ function addBookmarkButton(){
     const start=document.getElementsByClassName("flex items-start justify-between gap-4")[0]; 
 
     start.insertAdjacentElement("beforeend",bookmarkButton);
+
+    bookmarkButton.addEventListener("click",addnewBookmarkHandler);
 }
+async function addnewBookmarkHandler(){
+    const currrentBookmarks= await getCurrentBookmarks();
+
+    const problemURL=window.location.href;
+    const unid=extractTextBetween(problemURL);
+    const probname=document.getElementsByClassName("no-underline hover:text-blue-s dark:hover:text-dark-blue-s truncate cursor-text whitespace-normal hover:!text-[inherit]")[0].innerText;
+    
+    if(currrentBookmarks.some((bookmark)=>bookmark.id===unid))return;
+
+    const BookmarkOBJ={
+        id:unid,
+        name:probname,
+        url:problemURL
+    }
+    const updatedBookmarks = [...currrentBookmarks,BookmarkOBJ];
+    chrome.storage.sync.set({PROBLEM_KEY:updatedBookmarks},()=>{
+        console.log("Updated the bookmarks correctly to ",updatedBookmarks);
+    });
+}
+function extractTextBetween(url) {
+    const startMarker = "problems/";
+    const endMarker = "/description";
+    
+    const start = url.indexOf(startMarker);
+    const end = url.indexOf(endMarker);
+    
+    if (start !== -1 && end !== -1 && start < end) {
+        return url.slice(start + startMarker.length, end);
+    }
+    return null;
+}
+function getCurrentBookmarks(){
+    return new Promise((resolve,reject)=>{
+        chrome.storage.sync.get([PROBLEM_KEY],(results)=>{
+            resolve(results[PROBLEM_KEY]||[]);
+        });
+    });
+}
+
